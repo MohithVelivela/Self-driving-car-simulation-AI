2205eb06635ac1ec62ccad3b228b3e7ae8991c93
Eshwar <reshwar2004@gmail.com>
2024-02-29 21:44:11 +0530

    fixed car's collision shape

--- src/player.py
+++ src/player.py
@@ -2,16 +2,20 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, image):
         pygame.sprite.Sprite.__init__(self, self.containers)
 
         # Assigning all the player variable and initial setup
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (int(self.image.get_width() * 0.1),
+                                                                  int(self.image.get_height() *  0.1)))
+        
         self.width = 100
         self.height = 50
         self.vel = (0,0)
         self.speed = 5
 
-        self.rect = pygame.Rect(x, y, self.width, self.height)
+        self.rect = pygame.Rect(x, y, self.width(), self.height())
 
     def update(self, screen):
         # This function is called once a frame
@@ -26,7 +30,7 @@ class Player(pygame.sprite.Sprite):
         moved_hitbox = self.rect
 
         # Drawing the player
-        pygame.draw.rect(screen, (100, 100, 223), self.rect)
+        screen.blit(self.image, self.rect)
 
     def move(self):
         keys = pygame.key.get_pressed()
