61faedae02bf9323b657cf4d5d3ece46925a0c1f
Eshwar <reshwar2004@gmail.com>
2024-03-14 21:35:51 +0530

    improved movement

--- src/player.py
+++ src/player.py
@@ -7,7 +7,7 @@ import time
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, image, angle=0.0, length=4, max_steering=2, max_acceleration=4.0):
+    def __init__(self, x, y, image, angle=0.0, length=4, max_steering=2, max_acceleration=2.0):
         pygame.sprite.Sprite.__init__(self, self.containers)
 
         # Assigning all the player variable and initial setup
@@ -20,7 +20,7 @@ class Player(pygame.sprite.Sprite):
         self.length = length
         self.max_acceleration = max_acceleration
         self.max_steering = max_steering
-        self.max_velocity = 15
+        self.max_velocity = 25
         self.brake_deceleration = 5
         self.free_deceleration = 0.5
         self.acceleration = 0.0
